52f4c995495375ebf27e3cb5f924ed7352c4420d
Vanessa Ancken <v.ancken@gmail.com>
2018-08-29 09:31:23 -0300

    Banco de dados H2 e criação automática banco de dados

--- src/main/java/com/vanessa/cursomc/domain/Categoria.java
+++ src/main/java/com/vanessa/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.vanessa.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
