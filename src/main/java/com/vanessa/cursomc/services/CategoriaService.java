fd7c19299fa94668ce72163e39f2be97a0e05fec
Vanessa Ancken <v.ancken@gmail.com>
2018-08-30 11:23:03 -0300

    Ajustes no endpoint

--- src/main/java/com/vanessa/cursomc/services/CategoriaService.java
+++ src/main/java/com/vanessa/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.vanessa.cursomc.domain.Categoria;
 import com.vanessa.cursomc.repositories.CategoriaRepository;
+import com.vanessa.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = categoriaRepository.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado. Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
